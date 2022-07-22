3c4605176c4f9d11d5ad9913e0e665ad1774c0d0
kpopfan298 <christopher8664@gmail.com>
2022-07-22 16:12:22 +0800

    Fixed timer. Need to fix the Done button

--- src/screens/timer/WorkoutTimerPage.js
+++ src/screens/timer/WorkoutTimerPage.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { StyleSheet, Text, View,  TouchableOpacity, Dimensions, Pressable, Button } from 'react-native';
+import React, { useState, useEffect, useLayoutEffect, useCallback } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Pressable, Button } from 'react-native';
 import { secondToHHMMSS } from '../../utils/DateTimeUtil';
 import { useIsFocused } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 
 // Redux
-import {setCurrentWorkout} from '../../slices/currentWorkoutSlice'
+import { setCurrentWorkout } from '../../slices/currentWorkoutSlice'
 import { mapDocumentToUi } from '../../components/Card';
 
 export default function WorkoutTimerPage({ navigation }) {
   const dispatch = useDispatch();
   const currentWorkout = useSelector(state => state.currentWorkout)
-  const {mainTitle, content} = mapDocumentToUi(currentWorkout)
+  const { mainTitle, content } = mapDocumentToUi(currentWorkout)
 
   // const [timeState, setTimeState] = useState({
   //   startTime: null, 
@@ -58,31 +58,42 @@ export default function WorkoutTimerPage({ navigation }) {
 
   const reset = () => {
     console.log("-----reset")
-    setElapsedSeconds(0);
-    setIsActive(false) 
+    setIsActive(false)
+    setElapsedSeconds(0); 
     setTotalSeconds(0)
     clearInterval(intervalId)
   }
 
-  const completeWorkout = () => {
+  const completeWorkout = useCallback(() => {
+    console.log("-----Saving")
+    console.log("Total seconds", totalSeconds)
+    console.log("elapsedSeconds", elapsedSeconds)
     setIsActive(false)
-    dispatch(setCurrentWorkout({workoutDuration: totalSeconds + elapsedSeconds}))
+    dispatch(setCurrentWorkout({ workoutDuration: totalSeconds + elapsedSeconds }))
     navigation.navigate('Workout Summary')
-  }
+  }, [totalSeconds, elapsedSeconds])
+  // const completeWorkout = () => {
+  //   setIsActive(false)
+  //   console.log("-----Saving")
+  //   console.log("Total seconds", totalSeconds)
+  //   console.log("elapsedSeconds", elapsedSeconds)
+  //   dispatch(setCurrentWorkout({workoutDuration: totalSeconds + elapsedSeconds}))
+  //   navigation.navigate('Workout Summary')
+  // }
 
   // useEffect(() => {
   //   return () => clearInterval(interval)
   // })
 
-  useEffect(() => { 
+  useEffect(() => {
     // Start
     if (isActive) {
       const newStartTime = new Date()
-      interval = setInterval(()=> {
-        setElapsedSeconds(( new Date() - newStartTime) / 1000);
+      interval = setInterval(() => {
+        setElapsedSeconds((new Date() - newStartTime) / 1000);
       }, 1000);
       setIntervalId(interval)
-    // Pause or reset
+      // Pause or reset
     } else if (!isActive) {
       console.log("total seconds", totalSeconds)
       console.log("Elapsed seconds", elapsedSeconds)
@@ -103,6 +114,16 @@ export default function WorkoutTimerPage({ navigation }) {
     });
   }, [navigation])
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Pressable onPress={completeWorkout} style={{ marginRight: 26 }}>
+          <Text>Done</Text>
+        </Pressable>
+      ),
+    });
+  }, [navigation, startTime, elapsedSeconds])
+
   // When screen focuses
   // const isFocused = useIsFocused();
   // useEffect(() => {  
@@ -115,27 +136,37 @@ export default function WorkoutTimerPage({ navigation }) {
   //   }
   // }, [isFocused])
 
-  console.log("total seconds", totalSeconds)
-  console.log("Elapsed seconds", elapsedSeconds)
+  let workoutDuration = totalSeconds + elapsedSeconds
+
   return (
     <View style={styles.container}>
-      <View style={{flex: 2, padding: 30, alignItems: 'center'}}>
+      <View style={{ flex: 2, padding: 30, alignItems: 'center' }}>
         <Text>{mainTitle}</Text>
         <Text>{content}</Text>
       </View>
-      <View style={{flex: 3, justifyContent: 'space-evenly', alignItems: 'center'}}>
-      <Text style={styles.timerText}>{secondToHHMMSS(totalSeconds + elapsedSeconds)}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={toggle} style={styles.button}>
+      <View style={{ flex: 3, justifyContent: 'space-evenly', alignItems: 'center' }}>
+        <Text style={styles.timerText}>{secondToHHMMSS(workoutDuration)}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={toggle} style={styles.button}>
             <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={reset} style={[styles.button, styles.buttonReset]}>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={reset} style={[styles.button, styles.buttonReset]}>
             <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            console.log("-----Saving")
+            console.log("Total seconds", totalSeconds)
+            console.log("elapsedSeconds", elapsedSeconds)
+            setIsActive(false)
+            dispatch(setCurrentWorkout({ workoutDuration: workoutDuration }))
+            navigation.navigate('Workout Summary')
+          }} style={[styles.button, styles.buttonReset]}>
+            <Text style={[styles.buttonText, styles.buttonTextReset]}>Done</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      </View>
-      
-      
+
+
       {/* <Pressable onPress={completeWorkout}>
         <Text style={[styles.buttonText, styles.buttonTextReset]}>Done</Text>
       </Pressable> */}
@@ -154,22 +185,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   button: {
-      borderWidth: 10,
-      borderColor: '#B9AAFF',
-      borderRadius: 20,
-      alignItems: 'center',
-      justifyContent: 'center'
-  }, 
+    borderWidth: 10,
+    borderColor: '#B9AAFF',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   buttonText: {
-      fontSize: 45,
-      color: '#B9AAFF'
+    fontSize: 45,
+    color: '#B9AAFF'
   },
   timerText: {
-      fontSize: 60,
-      marginBottom: 20
+    fontSize: 60,
+    marginBottom: 20
   },
   buttonReset: {
-      borderColor: "#FF851B"
+    borderColor: "#FF851B"
   },
   buttonTextReset: {
     color: "#FF851B"
