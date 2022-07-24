97702ca224a6a1d5646467784951108400f5d47d
kpopfan298 <christopher8664@gmail.com>
2022-07-24 16:38:01 +0800

    Added workoutListSlice in Redux

--- src/slices/workoutListSlice.js
+++ src/slices/workoutListSlice.js
@@ -2,15 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 import { collection, addDoc, query, where, getDocs, deleteDoc, doc, setDoc } from "firebase/firestore"; 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getAuth } from 'firebase/auth';
-
+import { db } from "../../firebase";
 // https://hybridheroes.de/blog/2021-01-08-redux-toolkit-react-native/
 //https://redux-toolkit.js.org/usage/usage-guide#async-requests-with-createasyncthunk
 
 export const loadAllWorkouts = createAsyncThunk('workoutList/loadAllWorkouts', async () => {
-    const auth = getAuth()
-    const q = query(collection(db, "user_workouts"), where("userId", "==", auth.currentUser.uid));
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.map(doc => doc.data());
+  const auth = getAuth()
+  const q = query(collection(db, "user_workouts"), where("userId", "==", auth.currentUser.uid));
+  const querySnapshot = await getDocs(q);
+  let exerciseList = [];
+  querySnapshot.forEach((doc) => {
+    let workoutItem = doc.data();
+    workoutItem.workoutId = doc.id
+    exerciseList.push(workoutItem);
+  });  
+  return exerciseList
+  // Note: querySnapshot is not an array, DOES NOT have the map function
+  // return querySnapshot.map(doc => {
+  //   let item = doc.data()
+  //   item.id = doc.id
+  //   return item
+  // });
 });
 
 const workoutListSlice = createSlice({
